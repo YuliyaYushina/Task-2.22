c5ee71b1efe81a24d05af280ef82be087cf2ccb3
YuliyaYushina <ulia84400@gmail.com>
2024-10-08 17:36:55 +0300

    Task 2.20

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = new int[20];
+        List<Integer> numbers = intsInRange(20, 1, 16);
+        System.out.println(numbers);
+    }
 
+    public static List<Integer> intsInRange(int size, int lowerBound, int upperBound) {
+        Random random = new Random();
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            result.add(random.nextInt(upperBound - lowerBound) + lowerBound);
+        }
+        return result;
     }
 }
