25de88f3e81f622a3af1798bb34d187b0ea5bf8e
YuliyaYushina <ulia84400@gmail.com>
2024-10-30 23:11:52 +0300

    Task 2.21

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,45 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введите строку");
+        String mainString = sc.nextLine();
+        System.out.println("Введите подстроку");
+        String subString = sc.nextLine();
+        int count = countSubString(mainString, subString);
+        System.out.println("Подстрока '" + subString + "' встречается " + count + " раза");
+
+        System.out.println("Введите строку");
+        String replacementString = sc.nextLine();
+        String correctedString = replaceString(replacementString);
+        System.out.println(correctedString);
+
+        System.out.println("Введите дату в формате 'дд.мм.гггг'");
+
+
+    }
+
+    public static int countSubString(String mainString, String sudString) {
+        int count = 0;
+        int index = 0;
+
+        while ((index = mainString.indexOf(sudString, index)) != -1) {
+            count++;
+            index += sudString.length();
+        }
+
+        return count;
+    }
+
+    public static String replaceString(String replacementString) {
+        String swapString1 = "кака";
+        String swapString2 = "бяка";
+        String censorshipString = "вырезано цензурой";
+        replacementString = replacementString.replace(swapString1, censorshipString);
+        replacementString = replacementString.replace(swapString2, censorshipString);
+        return replacementString;
     }
 }
