3f7567dec276fb6d85e1ac9be1576a34f9901614
YuliyaYushina <ulia84400@gmail.com>
2024-10-30 23:58:31 +0300

    Task 2.21

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
         Scanner sc = new Scanner(System.in);
 
@@ -18,8 +21,12 @@ public class Main {
         System.out.println(correctedString);
 
         System.out.println("Введите дату в формате 'дд.мм.гггг'");
+        String dateString = sc.nextLine();
+        String correctDate = correctedDate(dateString);
+        System.out.println(correctDate);
 
-
+        String simpleDate = correctedSimpleDate(dateString);
+        System.out.println(simpleDate);
     }
 
     public static int countSubString(String mainString, String sudString) {
@@ -41,5 +48,21 @@ public class Main {
         replacementString = replacementString.replace(swapString1, censorshipString);
         replacementString = replacementString.replace(swapString2, censorshipString);
         return replacementString;
+    }
+
+    public static String correctedDate(String dateString) {
+        String correctString = dateString.substring(6, 10) + "-" +
+                             dateString.substring(3, 5) + "-" +
+                             dateString.substring(0, 2);
+        return correctString;
+    }
+
+    public static String correctedSimpleDate(String dateString) throws ParseException {
+        SimpleDateFormat inputFormat = new SimpleDateFormat("dd.MM.yyyy");
+        SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        Date date = inputFormat.parse(dateString);
+        String outputDate = outputFormat.format(date);
+        return outputDate;
     }
 }
