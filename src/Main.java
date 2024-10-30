81b05c9dd6530bdc3b9ce3e6ebce9903e53c8b64
YuliyaYushina <ulia84400@gmail.com>
2024-10-30 22:20:20 +0300

    Task 2.21

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,6 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int length = 20;
-        int min = 1;
-        int max = 15;
 
-        int[] randomNumbers = new int[length];
-
-        Random random = new Random();
-
-        for (int i = 0; i < length; i++) {
-            randomNumbers[i] = random.nextInt(max - min + 1) + min;
-        }
-
-        System.out.println(Arrays.toString(randomNumbers));
-
-        Map<Integer, Integer> countMap = new HashMap<>();
-        for (int number : randomNumbers) {
-            countMap.put(number, countMap.getOrDefault(number, 0) + 1);
-        }
-
-        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
-            if (entry.getValue() > 1) {
-                System.out.println("Число '" + entry.getKey() + "' встречается " + entry.getValue() + " раза");
-            }
-        }
     }
 }
